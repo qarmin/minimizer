159499f4acd46f86737dbae3a49dde9a228a8845
Rafał Mikrut <mikrutrafal@protonmail.com>
2024-08-01 00:49:31 +0200

    Minimizer

---
+++ src/data_trait.rs
@@ -0,0 +1,63 @@
+use std::fs;
+use strum_macros::Display;
+
+pub trait DataTraits<T> {
+    fn save_to_file(&self, file_name: &str) -> Result<(), std::io::Error>;
+    fn get_mut_vec(&mut self) -> &mut Vec<T>;
+    fn get_vec(&self) -> &Vec<T>;
+    fn len(&self) -> usize {
+        self.get_vec().len()
+    }
+}
+pub struct MinimizationBytes {
+    pub(crate) bytes: Vec<u8>,
+}
+impl DataTraits<u8> for MinimizationBytes {
+    fn save_to_file(&self, file_name: &str) -> Result<(), std::io::Error> {
+        fs::write(file_name, &self.bytes)
+    }
+    fn get_mut_vec(&mut self) -> &mut Vec<u8> {
+        &mut self.bytes
+    }
+    fn get_vec(&self) -> &Vec<u8> {
+        &self.bytes
+    }
+}
+pub struct MinimizationLines {
+    pub(crate) lines: Vec<String>,
+}
+impl DataTraits<String> for MinimizationLines {
+    fn save_to_file(&self, file_name: &str) -> Result<(), std::io::Error> {
+        fs::write(file_name, self.lines.join("\n"))
+    }
+    fn get_mut_vec(&mut self) -> &mut Vec<String> {
+        &mut self.lines
+    }
+    fn get_vec(&self) -> &Vec<String> {
+        &self.lines
+    }
+}
+pub struct MinimizationChars {
+    pub(crate) chars: Vec<char>,
+}
+impl DataTraits<char> for MinimizationChars {
+    fn save_to_file(&self, file_name: &str) -> Result<(), std::io::Error> {
+        fs::write(file_name, &self.chars.iter().collect::<String>())
+    }
+    fn get_mut_vec(&mut self) -> &mut Vec<char> {
+        &mut self.chars
+    }
+    fn get_vec(&self) -> &Vec<char> {
+        &self.chars
+    }
+}
+
+#[derive(Display)]
+pub enum Mode {
+    #[strum(serialize = "bytes")]
+    Bytes,
+    #[strum(serialize = "lines")]
+    Lines,
+    #[strum(serialize = "chars")]
+    Chars,
+}
---
+++ src/settings.rs
@@ -0,0 +1,64 @@
+use clap::Parser;
+
+#[derive(Parser)]
+#[command(name = "Files minimizator")]
+#[command(author = "Rafał Mikrut")]
+#[command(version = "1.0.0")]
+#[command(
+    about = "Minimize files",
+    long_about = "App that minimizes files, to find smallest possible file that have."
+)]
+pub struct Settings {
+    #[arg(
+        short,
+        long,
+        value_name = "INPUT",
+        help = "Input file that will be minimized"
+    )]
+    pub(crate) input_file: String,
+
+    #[arg(
+        short,
+        long,
+        value_name = "OUTPUT",
+        help = "Output file to save results"
+    )]
+    pub(crate) output_file: String,
+
+    #[arg(short, long, value_name = "NUMBER", help = "Attempts to minimize file")]
+    pub(crate) attempts: u32,
+
+    #[arg(
+        short,
+        long,
+        value_name = "NUMBER",
+        help = "Reset attempts counter to start value, when file was minimized in current iteration",
+        default_value_t = 100
+    )]
+    pub(crate) reset_attempts: u32,
+
+    #[arg(
+        short = 'u',
+        long,
+        value_name = "IS_CHARACTER_MODE",
+        help = "Operates on characters instead of bytes, will fail if file is not text file",
+        default_value_t = false
+    )]
+    pub(crate) character_mode: bool,
+
+    #[arg(
+        short,
+        long,
+        value_name = "COMMAND",
+        help = "Command which will be used to minimize e.g. 'godot {} -c 1000'\nBy default {} is used as placeholder for file, but this can be changed.\nAll occurrences of \" will be replaced with '"
+    )]
+    pub(crate) command: String,
+
+    #[arg(
+        short,
+        long,
+        value_name = "BROKEN_CONTENT",
+        help = "Content inside output of command, that will show that file is broken"
+    )]
+    pub(crate) broken_info: String,
+}
